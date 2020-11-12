51c914f6269ee0551138dbe67bc62ffde98d0f91
Muhammad Suardi <suardihaidar22@gmail.com>
2020-11-12 22:06:41 +0700

    add splash screen on android

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+import RNBootSplash from 'react-native-bootsplash';
 
 import RootView from '@/views';
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({duration: 250});
+  }, []);
+
   return (
     <NavigationContainer>
       <RootView />
