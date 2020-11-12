fad9bf4f4a870f487fcb919bb6df77515d462e6d
Muhammad Suardi <suardihaidar22@gmail.com>
2020-11-13 00:08:50 +0700

    add assets and color in index assets

---
+++ src/assets/index.js
@@ -0,0 +1,37 @@
+// initiate image from folder ./images
+import info from './images/info.png';
+import qa from './images/qa.png';
+import result from './images/result.png';
+import back from './images/back.png';
+
+// Color asset
+export const Color = {
+  black: '#000000',
+  blue: '#03A9F4',
+  darkBlue: '#0288D1',
+  green: '#7ED321',
+  darkGrey: '#7B7B7B',
+  grey: '#BDBDBD',
+  orange: '#FF9800',
+  red: '#F44336',
+  softGrey: '#E0E0E0',
+  white: '#FFFFFF',
+  lightGrey: '#efefef',
+  lightGreen: '#1AC774',
+  darkPurple: '#442F5B',
+  greenMountain: '#15C872',
+  softPurple: '#CDC5D3',
+  lightBlack: '#b2b2b2',
+  blueViking: '#5DBCCF',
+  vividRed: '#ff5131',
+  strongRed: '#b52424',
+  dark: '#434040',
+};
+
+// Image assets
+export const Assets = {
+  info,
+  qa,
+  result,
+  back,
+};
