7560ecd5984768099e70aed114963d04696bf27b
ozyzakarya <ozyzakarya@gmail.com>
2020-11-19 13:59:06 +0700

    setup firebase

---
+++ src/services/Firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+        apiKey: "AIzaSyA7jSywYNz8MzBGiimBJrs5ZvTVoBSOvyY",
+        authDomain: "questioner-app-51d1d.firebaseapp.com",
+        databaseURL: "https://questioner-app-51d1d.firebaseio.com",
+        projectId: "questioner-app-51d1d",
+        storageBucket: "questioner-app-51d1d.appspot.com",
+        messagingSenderId: "332979634402",
+        appId: "1:332979634402:web:55f1f5f1545ecb7904debb"
+})
+
+const Firebase = firebase;
+
+export default Firebase;
