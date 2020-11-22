17ffaf045074e37f1318e81447ff69610751837a
Muhammad Suardi <suardihaidar22@gmail.com>
2020-11-23 04:15:46 +0700

    add data question utils

---
+++ src/utils/dataQuestion.js
@@ -0,0 +1,143 @@
+const mainQuest = [
+  {
+    ID: 1,
+    question:
+      'Menyediakan sarana cuci tangan pakai sabun dengan air mengalir atau handsanitizer di pintu masuk',
+    point: 0,
+  },
+  {
+    ID: 2,
+    question:
+      'Mewajibkan pekerja/pengunjung untuk mencuci tangan atau menggunakan handsanitizer sebelum masuk',
+    point: 0,
+  },
+  {
+    ID: 3,
+    question:
+      'Melakukan dan mewajibkan pemeriksaan suhu di pintu masuk. Apabila ditemukan pekerja/pengunjung dengan suhu > 37,3ºC (2 kali pengukuran dengan jarak 5 menit) tidak diperkenankan masuk',
+    point: 0,
+  },
+  {
+    ID: 4,
+    question: 'Mewajibkan pengunjung untuk mengenakan masker',
+    point: 0,
+  },
+  {
+    ID: 5,
+    question:
+      'Mewajibkan pengaturan jarak minimal 1 meter pada saat antri masuk rumah makan/restoran',
+    point: 0,
+  },
+  {
+    ID: 6,
+    question: 'Pekerja mengenakan masker selama bekerja',
+    point: 0,
+  },
+  {
+    ID: 7,
+    question:
+      'Pekerja yang kontak langsung dengan makanan mengenakan masker, sarung tangan, dan atau penjepit pada saat menyentuh makanan siap saji',
+    point: 0,
+  },
+  {
+    ID: 8,
+    question:
+      'Pekerja yang mempersiapkan dan mengolah makanan mengenakan masker, sarung tangan dan atau penjepit, penutup kepala dan celemek',
+    point: 0,
+  },
+  {
+    ID: 9,
+    question:
+      'Jarak antar kursi minimal 1 meter dan tidak berhadapan, jika berhadapan menggunakan partisi kaca/mika/plastik di atas meja',
+    point: 0,
+  },
+  {
+    ID: 10,
+    question:
+      'Kursi tidak berhadapan, jika berhadapan menggunakan partisi kaca/mika/plastik di atas meja',
+    point: 0,
+  },
+  {
+    ID: 11,
+    question:
+      'Terdapat sirkulasi udara dan sinar matahari yang cukup atau filter AC bersih (jika menggunakan AC)',
+    point: 0,
+  },
+  {
+    ID: 12,
+    question:
+      'Alat makan yang diletakkan di meja makan (sendok, garpu atau pisau) ditutup bersih, misalnya dengan tissue atau diberikan kepada pengunjung apabila diminta',
+    point: 0,
+  },
+  {
+    ID: 13,
+    question:
+      'Peralatan makan yang sering disentuh diatas meja diganti dengan  alat makan sekali pakai/sachet',
+    point: 0,
+  },
+  {
+    ID: 14,
+    question:
+      'Mewajibkan pengaturan jarak minimal 1 meter pada saat antri memesan dan membayar dengan tanda di lantai',
+    point: 0,
+  },
+  {
+    ID: 15,
+    question:
+      'Terdapat pembatas (plastik/mika/kaca) antara pengunjung dengan meja pesanan/kasir',
+    point: 0,
+  },
+  {
+    ID: 16,
+    question:
+      'Pembersihan dan desinfeksi minimal 3 kali sehari untuk permukaan area dan peralatan yang sering di sentuh, seperti kran dan kenop pintu',
+    point: 0,
+  },
+  {
+    ID: 17,
+    question:
+      'Pembersihan dan Desinfeksi rumah makan minimal 2 kali sehari, saat sebelum buka dan tutup',
+    point: 0,
+  },
+  {
+    ID: 18,
+    question:
+      'Adanya pelayanan pemesanan makanan dan minuman secara online atau delivery service atau drive thru',
+    point: 0,
+  },
+];
+
+const additionalQuest = [
+  {
+    ID: 1,
+    question:
+      'Rumah Makan/Restoran memiliki Sertifikat laik higiene sanitasi atau stiker pembinaan dari Puskesmas/Dinas Kesehatan/KKP',
+    answer: 'no',
+  },
+  {
+    ID: 2,
+    question:
+      'Rumah Makan/Restoran memiliki kebijakan atau komitmen tertulis penerapan protokol kesehatan adaptasi kebiasaan baru di rumah makan',
+    answer: 'no',
+  },
+  {
+    ID: 3,
+    question:
+      'Rumah Makan /Restoran memiliki Standar Operasional Prosedur (SOP) penerapan protokol kesehatan adaptasi kebiasaan baru di rumah makan',
+    answer: 'no',
+  },
+  {
+    ID: 4,
+    question:
+      'Pengelola memberikan pemahaman kepada pekerja tentang covid 19 dan pencegahannya',
+    answer: 'no',
+  },
+  {
+    ID: 5,
+    question:
+      'Terdapat media sosialisasi penerapan protokol kesehatan adaptasi kebiasaan baru pada pengunjung',
+    answer: 'no',
+  },
+];
+
+export {mainQuest, additionalQuest};
