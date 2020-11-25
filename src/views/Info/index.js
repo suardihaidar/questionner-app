6a0abac06e29d22864f37d6ecfe1f9a60f0de4c7
Muhammad Suardi <suardihaidar22@gmail.com>
2020-11-26 02:29:09 +0700

    tweak info screen

--- src/views/Info/index.js
+++ src/views/Info/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View, FlatList, Image, Dimensions} from 'react-native';
+
+import {Assets} from '@/assets';
 
 const Info = () => {
+  const image = [Assets.E, Assets.D, Assets.C, Assets.B, Assets.A];
+
+  const renderItem = ({item}) => {
+    const {width, height} = Dimensions.get('window');
+
+    return (
+      <View
+        style={{
+          marginHorizontal: 10,
+          marginTop: 5,
+          marginBottom: -70,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Image
+          source={item}
+          style={{
+            height: height * 0.9,
+            width: width * 0.9,
+            resizeMode: 'contain',
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
-    <SafeAreaView
-      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Coming Soon</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <FlatList
+        data={image}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 };
