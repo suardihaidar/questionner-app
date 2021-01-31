27be864194f36fecfe11740a4f514b2050b21e16
Muhammad Suardi <suardihaidar22@gmail.com>
2021-02-01 00:45:06 +0700

    hide firebase credentials

--- src/services/Firebase/index.js
+++ src/services/Firebase/index.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
+import Config from 'react-native-config';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyA7jSywYNz8MzBGiimBJrs5ZvTVoBSOvyY',
-  authDomain: 'questioner-app-51d1d.firebaseapp.com',
-  databaseURL: 'https://questioner-app-51d1d.firebaseio.com',
-  projectId: 'questioner-app-51d1d',
-  storageBucket: 'questioner-app-51d1d.appspot.com',
-  messagingSenderId: '332979634402',
-  appId: '1:332979634402:web:55f1f5f1545ecb7904debb',
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  databaseURL: Config.FIREBASE_DATABASE_URL,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID,
 });
 
 export default firebase;
